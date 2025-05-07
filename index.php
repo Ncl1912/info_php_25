7c09c5c7c91d856b11e9ab245c4a6d28cb21aff8
suporte <suporte@BG-LABX-PCX>
2025-05-06 21:29:03 -0300

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Ola Mundo! <br>";
+// \n = quebra de linha - terminal
+// <br> = quebra de linha - navegador
+
+//echo 2 + 2; 
+//  comentário de linha = // ; comentário de bloco = /* ... */
+
+$x = 2 + 2; 
+
+echo $x ; 
+
+echo "<br>";
+
+$x = 20 + 20 ;
+
+echo $x; 
+
+
+
+
